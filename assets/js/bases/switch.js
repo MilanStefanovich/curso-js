d8300e5b675fe769004d8e5333d68ba98b26d201
Milan Stefanovich <stefanovich60@gmail.com>
2022-01-05 15:18:46 -0300

    termino de la seccion 4

---
+++ assets/js/bases/switch.js
@@ -0,0 +1,15 @@
+const dia = 1;
+
+switch (dia) {
+    case 0:
+        console.log('domingo');
+        break;
+    case 1:
+        console.log('lunes')
+        break;
+    case 2:
+        console.log('martes')
+        break;
+    default:
+        console.log('dia de la semana no reconocido')
+}
---
+++ assets/js/ciclos/while-do-while.js
@@ -0,0 +1,18 @@
+
+console.warn(' while')
+const carros = ['ford', 'mazda', 'kia'];
+
+let i = 0;
+while (i < carros.length) {
+    console.log(carros[i])
+    i++;
+}
+
+console.warn('do while')
+
+let j = 0;
+
+do{
+    console.log(carros[j])
+    i++;
+}while (carros[j]);
